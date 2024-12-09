8d6cfc6766b3f34e06df4805f97bb7e6702f94a7
LockBorn1 <161664739+LockBorn1@users.noreply.github.com>
2024-12-09 13:20:21 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed by Tyler Payne Developed by Gage Milner</h3>
+          <h3>Designed by Tyler Payne, Developed by Gage Milner</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Gage Milner</h3>
