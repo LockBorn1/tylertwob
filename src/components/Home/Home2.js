c8c8b423527d89b092673bd05509721e71c0f5d5
LockBorn1 <161664739+LockBorn1@users.noreply.github.com>
2024-12-11 07:42:26 -0500

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -18,35 +19,50 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I am a bold character who defies societal norms and embraces my own desires.
+              I fell in love with programming and I have at least learnt
+              something, I think… 🤷‍♂️
               <br />
-              <br />
-              I am fluent in classics like
+              <br />I am fluent in classics like
               <i>
-                <b className="purple"> the knowledge to challenge and empower myself in a male-dominated world. </b>
+                <b className="purple"> C++, Javascript and Go. </b>
               </i>
               <br />
               <br />
+              My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">JavaScript</b> and also in areas related to{" "}
-                <b className="purple">Node.js</b>.
+                <b className="purple">Web Technologies and Products </b> and
+                also in areas related to{" "}
+                <b className="purple">
+                  Blockchain.
+                </b>
               </i>
               <br />
               <br />
+              Whenever possible, I also apply my passion for developing products
+              with <b className="purple">Node.js</b> and
               <i>
-                <b className="purple">React</b>, <b className="purple">Frontend Development</b>, and
-                <b className="purple">Backend Technologies</b> are some of my strong areas.
+                <b className="purple">
+                  {" "}
+                  Modern Javascript Library and Frameworks
+                </b>
               </i>
-              <br />
-              <br />
+              &nbsp; like
+              <i>
+                <b className="purple"> React.js and Next.js</b>
+              </i>
             </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
           </Col>
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect</span> with me
+              Feel free to <span className="purple">connect </span>with me
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
@@ -54,7 +70,7 @@ function Home2() {
                   href="https://github.com/soumyajit4419"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour home-social-icons"
+                  className="icon-colour  home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
@@ -64,7 +80,7 @@ function Home2() {
                   href="https://twitter.com/Soumyajit4419"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour home-social-icons"
+                  className="icon-colour  home-social-icons"
                 >
                   <AiOutlineTwitter />
                 </a>
@@ -74,7 +90,7 @@ function Home2() {
                   href="https://www.linkedin.com/in/soumyajit4419/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour home-social-icons"
+                  className="icon-colour  home-social-icons"
                 >
                   <FaLinkedinIn />
                 </a>
@@ -96,5 +112,4 @@ function Home2() {
     </Container>
   );
 }
-
 export default Home2;
